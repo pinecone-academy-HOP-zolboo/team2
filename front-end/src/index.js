f410339e8bace3a8df545b932f6c22bdf879276c
Fulxe <muruuuu8@gmail.com>
2023-02-16 17:08:49 +0800

    test

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Post from "../src/Components/Post/post";
-import App from "../src/App";
+import Post from "./Components/Post/post";
+import App from "./App";
 import { Blog } from "./Components/Blog/blog";
 import Navbar from "./Components/Navbar/navbar";
 
